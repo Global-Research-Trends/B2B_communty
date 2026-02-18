019040616417dbcecc05bb77e79aa4ce14217587
unknown <fatima-batool631@users.noreply.github.com>
2026-02-18 15:52:18 +0500

    Updated file paths

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 ﻿﻿import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../../../B2B/src/components/Header/Header';
-import Footer from '../../../B2B/src/components/Footer/Footer';
+import Header from '../../components/Header/Header.tsx';
+import Footer from '../../components/Footer/Footer.tsx';
 import './Home.css';
 
 const Home: React.FC = () => {
